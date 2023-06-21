a398d635f7f3ee5f6b52608c20ba8ec0a35d632c
Manav verma <129318380+vermamanav23@users.noreply.github.com>
2023-06-21 13:46:03 +0530

    added index.html

---
+++ main.js
@@ -0,0 +1,46 @@
+// change navbar styles on scroll
+
+window.addEventListener('scroll',() => {
+
+     document.querySelector('nav').classList.toggle
+     ('window-scroll',window.scrollY > 100)
+
+})
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click',() => {
+       faq.classList.toggle('open');
+
+       const icon = faq.querySelector('faq__icon i');
+       if(icon.className === 'fas fa-plus'){
+        icon.className = "fas fa-minus";
+       }else{
+        icon.className = "fas fa-plus";
+       }
+    })
+})
+
+
+// navmenu
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display ="flex";
+    closeBtn.style.display ="inline-block";
+    menuBtn.style.display="none";
+})
+
+
+const closeNav= () => {
+    menu.style.display= "none";
+    closeBtn.style.display ="none";
+    menuBtn.style.display="inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
